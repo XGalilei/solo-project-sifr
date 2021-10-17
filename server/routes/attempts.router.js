091fdc92bc3728fd60830b4a89567c74ecd1f0a7
XGalilei <ianbush1@gmail.com>
2021-10-17 14:18:54 -0500

    Added in routes for attempts

--- server/routes/attempts.router.js
+++ server/routes/attempts.router.js
@@ -9,8 +9,19 @@ const router = express.Router();
  * GET the number of attempts for the challenge with the
  * specified ID
  */
-router.get('/challenge/:id', (req, res) => {
+router.get('/challenge-total/:id', (req, res) => {
   // GET route code here
+  const queryText = `SELECT * FROM "attempts" 
+  WHERE "challenge_id" = $1;`;
+});
+
+/**
+ * GET the number of successful attempts for the challenge
+ * with the specificied ID
+ */
+router.get('/challenge-success/:id', (req, res) => {
+  const queryText = `SELECT * FROM "attempts"
+  WHERE "challenge_id" = $1 AND "success" = true;`;
 });
 
 /**
@@ -24,8 +35,20 @@ router.get('/user/:id', (req, res) => {
 /**
  * POST route template
  */
-router.post('/', (req, res) => {
+router.post('/:id', (req, res) => {
   // POST route code here
+  const queryText = `INSERT INTO "attempts" ("success", "user_id", "challenge_id")
+  VALUES ($1, $2, $3);`;
+  const success = req.body.success;
+  const user_id = req.user.id;
+  const challenge_id = req.body.challenge;
+  pool.query(queryText, [success, user_id, challenge_id])
+  .then(result => {
+    res.sendStatus(200);
+  }).catch(error => {
+    console.log('Error in /POST attempt:', error);
+    res.sendStatus(500);
+  })
 });
 
 module.exports = router;
